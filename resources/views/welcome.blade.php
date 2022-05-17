4be24825753a6b19867a4eccf67fcdc33ca25695
ridan1411 <ridan.1411@gmail.com>
2022-05-17 01:35:18 -0500

    ultimos cambios

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -174,7 +174,14 @@
     <!-- Footer-->
     <footer class="py-5 bg-black">
         <div class="container px-5">
-            <p class="m-0 text-center text-white small">Copyright &copy; Your Website 2022</p>
+            <p class="m-0 text-center text-white small">Seminario de Titulación 2022</p>
+            <p class="m-0 text-white small">Copyright &copy;</p>
+            <ul class="m-0 text-white small">
+                <li>Cinthya Arellano</li>
+                <li>Jordan Rubio</li>
+                <li>Carlos Ridan</li>
+                <li>Alan Iñiguez</li>
+            </ul>
         </div>
     </footer>
     <!-- Bootstrap core JS-->
